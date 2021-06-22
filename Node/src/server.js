55df51aace67da36508cf662a9c19aa9d41b31c6
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-06-22 22:06:47 +0000

    Add Homework for M5-D2

---
+++ Node/src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+
+import cors from "cors"
+
+import listEndpoints from "express-list-endpoints"
+
+const server = express();
+const PORT = 3001;
+
+server.use(cors())
+
+server.use(express.json())
+
+console.log(listEndpoints(server));
+
+server.use(PORT,() => console.log(`Server is running! on port : ${PORT}`))
+
+server.on('error',(error)=>
+console.log(`Server is not running due to : ${error}`)
+)
